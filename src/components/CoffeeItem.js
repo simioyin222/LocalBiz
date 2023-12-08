9cb6233c59e87281446af667087feb2296e10ad9
Simi O <coolkid1984@ymail.com>
2023-12-08 14:02:23 -0600

    coffee item.js complete with props of clicking and roast and price and origin

--- src/components/CoffeeItem.js
+++ src/components/CoffeeItem.js
@@ -1,3 +1,13 @@
 import React from 'react';
 
+function CoffeeItem(props) {
+  return (
+    <div onClick={() => props.whenCoffeeClicked(props.coffee.id)}>
+      <h3>{props.coffee.name} - {props.coffee.roast}</h3>
+      <p>{props.coffee.origin}</p>
+      <p>{props.coffee.price} per lb</p>
+    </div>
+  );
+}
 
+export default CoffeeItem;
