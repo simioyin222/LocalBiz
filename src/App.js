98530bfc5fe6d26048cf1b00e82945d6ac2f030d
Simi O <coolkid1984@ymail.com>
2023-12-08 16:25:19 -0600

    more essential functioning

--- src/App.js
+++ src/App.js
@@ -3,4 +3,12 @@ import Header from './components/Header';
 import CoffeeList from './components/CoffeeList';
 import NewCoffeeForm from './components/NewCoffeeForm';
 
+function App() {
+  const [mainCoffeeList, setMainCoffeeList] = useState([]);
+  const [selectedCoffee, setSelectedCoffee] = useState(null);
+
+  
+
+}
+
 export default App;
