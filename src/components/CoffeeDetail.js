0df7d7a8ecd53fb14cb3bf9a6d3599f5e224ba2f
Simi O <coolkid1984@ymail.com>
2023-12-08 18:20:52 -0600

    add new docs folder for draw io import of diagram

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+function CoffeeDetail(props) {
+  return (
+    <React.Fragment>
+      <h1>Coffee Detail</h1>
+      <h3>{props.coffee.name} - {props.coffee.roast}</h3>
+      <p>Origin: {props.coffee.origin}</p>
+      <p>Price: {props.coffee.price} per lb</p>
+      <p>Pounds Remaining: {props.coffee.pounds}</p>
+      <button onClick={() => props.onClickingDecrement(props.coffee.id)}>Sell 1 lb</button>
+      <hr />
+    </React.Fragment>
+  );
+}
+
