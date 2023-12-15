20fae39458a7734f08f7f8072b42f577937cad1e
Simi O <coolkid1984@ymail.com>
2023-12-15 10:03:38 -0600

    add new prop types

--- src/components/CoffeeList.js
+++ src/components/CoffeeList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import CoffeeItem from './CoffeeItem';
+import PropTypes from 'prop-types';
 
 function CoffeeList(props) {
+  console.log("Coffee keys:", props.coffeeList.map(coffee => coffee.id)); // Debugging here 
+
   return (
     <div>
       {props.coffeeList.map((coffee) => 
@@ -13,5 +16,10 @@ function CoffeeList(props) {
     </div>
   );
 }
+
+CoffeeList.propTypes = {
+  coffeeList: PropTypes.array.isRequired,
+  onCoffeeSelection: PropTypes.func.isRequired
+};
 
 export default CoffeeList;
