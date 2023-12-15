a1563571e2c677465f057fffd5ad473972bdb59c
Simi O <coolkid1984@ymail.com>
2023-12-15 17:29:57 -0600

    first desrcibe and test

--- src/redux/__tests__/coffeeReducer.test.js
+++ src/redux/__tests__/coffeeReducer.test.js
@@ -1,3 +1,14 @@
 import coffeeReducer from '../reducers/coffeeReducer';
 import * as actions from '../actions';
 
+describe('coffeeReducer', () => {
+  test('Should return default state if no action type is recognized', () => {
+    expect(coffeeReducer(undefined, { type: null })).toEqual({
+      coffeeList: [],
+      selectedCoffee: null
+    });
+  });
+
+  // place where i would add more test for myself
+});
+
