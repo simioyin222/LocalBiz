b2deabd8ae029b87d38271342f630b5204de9893
Simi O <coolkid1984@ymail.com>
2023-12-15 17:33:30 -0600

    change first test to better align

--- src/redux/__tests__/coffeeReducer.test.js
+++ src/redux/__tests__/coffeeReducer.test.js
@@ -3,12 +3,11 @@ import * as actions from '../actions';
 
 describe('coffeeReducer', () => {
   test('Should return default state if no action type is recognized', () => {
-    expect(coffeeReducer(undefined, { type: null })).toEqual({
-      coffeeList: [],
-      selectedCoffee: null
-    });
+    coffeeList: [
+      { id: 1, name: 'Arabica', origin: 'Ethiopia', price: '15', roast: 'Medium', pounds: 130 },
+      { id: 2, name: 'Robusta', origin: 'Vietnam', price: '12', roast: 'Dark', pounds: 150 },
+    ],
+    selectedCoffee: null
   });
-
-  // place where i would add more test for myself
 });
 
