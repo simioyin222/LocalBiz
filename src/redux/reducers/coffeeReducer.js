2ca931e0050e0d20d8cfc9e979cf824e3cadc69c
Simi O <coolkid1984@ymail.com>
2023-12-15 13:25:12 -0600

    add finalization on reducer function

--- src/redux/reducers/coffeeReducer.js
+++ src/redux/reducers/coffeeReducer.js
@@ -2,3 +2,11 @@ const initialState = {
   coffeeList: [],
   selectedCoffee: null
 };
+
+export default function coffeeReducer(state = initialState, action) {
+  switch (action.type) {
+    //  Use this later to define more cases for different actions here (note to self)
+    default:
+      return state;
+  }
+}
