76b3d31737c2193419e8d5fa82d44851caa59ea5
Simi O <coolkid1984@ymail.com>
2023-12-15 17:51:02 -0600

    fix test and add new test for adding of new coffee

--- src/redux/__tests__/coffeeReducer.test.js
+++ src/redux/__tests__/coffeeReducer.test.js
@@ -3,11 +3,19 @@ import * as actions from '../actions';
 
 describe('coffeeReducer', () => {
   test('Should return default state if no action type is recognized', () => {
-    coffeeList: [
-      { id: 1, name: 'Arabica', origin: 'Ethiopia', price: '15', roast: 'Medium', pounds: 130 },
-      { id: 2, name: 'Robusta', origin: 'Vietnam', price: '12', roast: 'Dark', pounds: 150 },
-    ],
-    selectedCoffee: null
+    expect(coffeeReducer(undefined, { type: null })).toEqual({
+      coffeeList: [
+        { id: 1, name: 'Arabica', origin: 'Ethiopia', price: '15', roast: 'Medium', pounds: 130 },
+        { id: 2, name: 'Robusta', origin: 'Vietnam', price: '12', roast: 'Dark', pounds: 150 },
+      ],
+      selectedCoffee: null
+    });
+  });
+
+  test('Should correctly add a new coffee to the coffeeList', () => {
+    const newCoffee = { id: 3, name: 'Test Coffee', origin: 'Test Origin', price: '10', roast: 'Light', pounds: 100 };
+    const action = actions.addCoffee(newCoffee);
+    const newState = coffeeReducer(undefined, action);
+    expect(newState.coffeeList).toContainEqual(newCoffee);
   });
 });
-
