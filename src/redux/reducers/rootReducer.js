a22457dfa91e9b2cc1840eb1c6e3788704f95379
Simi O <coolkid1984@ymail.com>
2023-12-15 14:20:49 -0600

    add recuer function of root reducers

---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import coffeeReducer from './coffeeReducer';
+
+const rootReducer = combineReducers({
+  coffee: coffeeReducer
+});
+
+export default rootReducer;
