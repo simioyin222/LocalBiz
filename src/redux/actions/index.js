8206c769ff1cd5522bd7f7ada74a6ebbb763f545
Simi O <coolkid1984@ymail.com>
2023-12-15 15:33:22 -0600

    add second action function to project

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,3 +2,8 @@ export const addCoffee = (coffee) => ({
   type: 'ADD_COFFEE',
   payload: coffee
 });
+
+export const selectCoffee = (coffeeId) => ({
+  type: 'SELECT_COFFEE',
+  payload: coffeeId
+});
