7d64f08511159ce976adaaacbb79dcc6ce1902e5
Simi O <coolkid1984@ymail.com>
2023-12-15 18:36:23 -0600

    remove remind message

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -3,7 +3,6 @@ import coffeeReducer from './coffeeReducer';
 
 const rootReducer = combineReducers({
   coffee: coffeeReducer
-  // Add more reducers here as your app grows
 });
 
 export default rootReducer;
