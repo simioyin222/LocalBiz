9555d01edd95476ab7c623fa05965e0b445edeb4
Simi O <coolkid1984@ymail.com>
2024-01-02 22:03:32 -0600

    change newcoffeeform functionality and and set nothing variable

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -19,33 +19,28 @@ function NewCoffeeForm(props) {
   }, [props.coffee]);
 
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const coffeeData = { name, origin, price, roast, pounds };
+
+    if (props.isEditing) {
+      props.onUpdateCoffee({ ...props.coffee, ...coffeeData });
+    } else {
+      props.onNewCoffeeCreation(coffeeData);
+    }
+
+    setName('');
+    setOrigin('');
+    setPrice('');
+    setRoast('');
+    setPounds('');
+  }
+
   return (
-    <form onSubmit={handleNewCoffeeFormSubmission}>
-      <input
-        type='text'
-        name='name'
-        placeholder='Coffee Name'
-        value={name}
-        onChange={(e) => setName(e.target.value)} />
-      <input
-        type='text'
-        name='origin'
-        placeholder='Origin'
-        value={origin}
-        onChange={(e) => setOrigin(e.target.value)} />
-      <input
-        type='text'
-        name='price'
-        placeholder='Price'
-        value={price}
-        onChange={(e) => setPrice(e.target.value)} />
-      <input
-        type='text'
-        name='roast'
-        placeholder='Roast'
-        value={roast}
-        onChange={(e) => setRoast(e.target.value)} />
-      <button type='submit'>Add Coffee</button>
+    <form onSubmit={handleSubmit}>
+      <h2>{props.isEditing ? 'Edit Coffee' : 'Add New Coffee'}</h2>
+      {}
+      <button type='submit'>{props.isEditing ? 'Update' : 'Add'}</button>
     </form>
   );
 }
